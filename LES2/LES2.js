08fcd9363247b9d8b5b56ca6acb6fa076e23b58a
NinjaNube008 <logan.houston4509@gmail.com>
2018-09-04 20:04:46 -0500

    Added LES2 and made LES1 WAYYYY more compact

---
+++ LES2/LES2.js
@@ -0,0 +1,30 @@
+var output;
+var chars = "1234567890abcdefghijklmnopqrstuvwxyz";
+var letters = "1234567890abcdefghijklmnopqrstuvwxyz";
+var static = "1234567890abcdefghijklmnopqrstuvwxyz";
+
+var chars = chars.split("");
+var letters = letters.split("");
+var static = static.split("");
+
+for (var i = 0; i < letters.length; i++) {
+    rand = Math.round(Math.random() * (chars.length - 1));
+    letters[i] = chars[rand];
+    chars.splice(rand, 1);
+}
+
+function LESEncode(idOfObj) {
+    var arr = document.getElementById(idOfObj).value.split("");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = letters[static.indexOf(arr[i], 0)];
+    }
+    output = arr.join("");
+}
+
+function LESDecode(idOfObj) {
+    var arr = document.getElementById(idOfObj).value.split("");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = static[letters.indexOf(arr[i], 0)];
+    }
+    output = arr.join("");
+}
